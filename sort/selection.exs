62a454898c4099452580f83e6626cca9115f5369
gentom <gento.m.developer@gmail.com>
2018-05-08 04:51:26 +0900

    add selection sort

---
+++ sort/selection.exs
@@ -0,0 +1,25 @@
+defmodule Selection do
+  def sort(list) when is_list(list) do
+    do_selection(list, [])
+  end
+
+  def do_selection([head|[]], acc) do
+    acc ++ [head]
+  end
+  def do_selection(list, acc) do
+    min = min(list)
+    do_selection(:lists.delete(min, list), acc ++ [min])
+  end
+
+  defp min([first|[second|[]]]) do
+    smaller(first, second)
+  end
+  defp min([first|[second|tail]]) do
+    min([smaller(first, second)|tail])
+  end
+
+  defp smaller(e1, e2) do
+    if e1 <= e2 do e1 else e2 end
+  end
+end
+IO.inspect Selection.sort([69,44,10,6,9,3,32,7,49,1,4])
