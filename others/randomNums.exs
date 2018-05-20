174a8b636ec6499ebedf1f1a2d05950fb145f7f7
gentom <gento.m.developer@gmail.com>
2018-05-20 20:06:40 +0900

    meaningless

---
+++ others/randomNums.exs
@@ -0,0 +1,12 @@
+defmodule Recursion do
+  def print_multiple_times(n) when n <= 1 do
+    IO.puts Enum.random(0..1000)
+  end
+
+  def print_multiple_times(n) do
+    IO.puts Enum.random(0..1000)
+    print_multiple_times(n - 1)
+  end
+end
+
+Recursion.print_multiple_times(50)
