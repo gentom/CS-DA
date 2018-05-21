cef26f1036c8cc90ada606aae83d5fd111200369
gentom <gento.m.developer@gmail.com>
2018-05-21 18:52:25 +0900

    add random num array generator

--- sort/selection.exs
+++ sort/selection.exs
@@ -24,6 +24,19 @@ defmodule Selection do
   end
 end
 '''
+defmodule RNAGen do
+  def random_int_array(length, list \\ [])
+  def random_int_array(0, list), do: list
+  def random_int_array(length, list) do
+    length - 1
+    |> random_int_array([random_int_num() | list])
+  end
+
+  defp random_int_num() do
+    Enum.random(0..100)
+  end
+end
+
 defmodule Selection do
 	def sort([]), do: []
 
@@ -52,4 +65,7 @@ defmodule Selection do
 end
 
 
-IO.inspect Selection.sort([69,44,10,6,9,3,32,7,49,1,4])
+rlist = RNAGen.random_int_array(20)
+IO.inspect rlist
+IO.inspect Selection.sort(rlist)
+
